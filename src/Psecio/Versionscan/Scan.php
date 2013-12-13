4a0b56bc311af435d5a1ebbfd3e74488c5863589
Chris Cornutt <ccornutt@mashery.com>
2013-12-13 16:07:03 -0600

    adding execute method to Scan class

--- src/Psecio/Versionscan/Scan.php
+++ src/Psecio/Versionscan/Scan.php
@@ -4,5 +4,16 @@ namespace Psecio\Versionscan;
 
 class Scan
 {
+    private $checks = array();
 
+    public function execute()
+    {
+        // pull in the Scan checks
+        $checks = json_decode(file_get_contents(__DIR__.'/checks.json'));
+        print_r($checks);
+        foreach ($checks->checks as $check) {
+            $this->checks[] = new \Psecio\Versionscan\Check($check);
+        }
+        print_r($this->checks);
+    }
 }
