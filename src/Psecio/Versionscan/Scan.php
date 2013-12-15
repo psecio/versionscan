f121a41ddede616c8cc30aa4b9a8cd46ccaec30a
enygma <enygma@phpdeveloper.org>
2013-12-14 18:52:33 -0600

    updating execute to call isVulnerable and add getChecks

--- src/Psecio/Versionscan/Scan.php
+++ src/Psecio/Versionscan/Scan.php
@@ -6,14 +6,24 @@ class Scan
 {
     private $checks = array();
 
-    public function execute()
+    public function execute($phpVersion = null)
     {
+        $phpVersion = ($phpVersion === null) ? PHP_VERSION : $phpVersion;
+
         // pull in the Scan checks
         $checks = json_decode(file_get_contents(__DIR__.'/checks.json'));
-        print_r($checks);
-        foreach ($checks->checks as $check) {
-            $this->checks[] = new \Psecio\Versionscan\Check($check);
+
+        foreach ($checks->checks as $index => $check) {
+            $check = new \Psecio\Versionscan\Check($check);
+            $result = $check->isVulnerable($phpVersion);
+            $check->setResult($result);
+
+            $this->checks[] = $check;
         }
-        print_r($this->checks);
+    }
+
+    public function getChecks()
+    {
+        return $this->checks;
     }
 }
