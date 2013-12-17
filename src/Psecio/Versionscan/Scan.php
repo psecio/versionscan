9c462affe6b4c4542a0528e6cba64d037307b4b1
Chris Cornutt <ccornutt@mashery.com>
2013-12-17 13:39:31 -0600

    updating the way checks are loaded to make testing easier

--- src/Psecio/Versionscan/Scan.php
+++ src/Psecio/Versionscan/Scan.php
@@ -11,60 +11,71 @@ class Scan
     private $checks = array();
 
     /**
-     * Execute the scan
-     * 
-     * @param type $phpVersion Optional PHP version
+     * The PHP version set for current checking
+     * @var string
      */
-    public function execute($phpVersion = null)
+    private $phpVersion = null;
+
+    /**
+     * Execute the scan
+     *
+     * @param type $phpVersion Optional PHP version
+     * @param mixed $checks Check information (either an array or null)
+     */
+    public function execute($phpVersion = null, $checks = null)
     {
-        $phpVersion = ($phpVersion === null) ? PHP_VERSION : $phpVersion;
+        $this->phpVersion = ($phpVersion === null) ? PHP_VERSION : $phpVersion;
 
         // pull in the Scan checks
-        $checks = $this->loadChecks();
-        $results = array();
-        foreach ($checks->checks as $index => $check) {
-            $check = new \Psecio\Versionscan\Check($check);
-            $result = $check->isVulnerable($phpVersion);
-            $check->setResult($result);
-
-            $results[] = $check;
-        }
-        $this->setChecks($results);
+        $this->loadChecks($checks);
     }
 
     /**
-     * Load the checks from the JSON file
-     * 
+     * Load the checks
+     *     If null is given as input, it loads from the file
+     *     If an array is given, it uses that data
+     *
+     * @param mixed $checks Check information
      * @return object Configuration loaded as an object
      */
-    public function loadChecks()
+    public function loadChecks($checks = null)
     {
-        // pull in the Scan checks
-        $path = __DIR__.'/checks.json';
-        if (is_file($path)) {
-            $checks = @json_decode(file_get_contents($path));
-            if ($checks === false) {
-                throw new Exception('Invalid check configuration');
+        if ($checks === null) {
+            // pull in the Scan checks
+            $path = __DIR__.'/checks.json';
+            if (is_file($path)) {
+                $checks = @json_decode(file_get_contents($path));
+                if ($checks === false) {
+                    throw new Exception('Invalid check configuration');
+                }
+                $this->setChecks($checks->checks);
+            } else {
+                throw new Exception('Could not load check file '.$path);
             }
-            return $checks;
-        } else {
-            throw new Exception('Could not load check file '.$path);
+        } elseif (is_array($checks)) {
+            $this->setChecks($checks);
         }
     }
 
     /**
      * Set the results of the check evaluation
-     * 
+     *
      * @param array $checks Set of check evaluation results
      */
     public function setChecks(array $checks)
     {
-        $this->checks = $checks;
+        foreach ($checks as $index => $check) {
+            $check = new \Psecio\Versionscan\Check($check);
+            $result = $check->isVulnerable($this->phpVersion);
+            $check->setResult($result);
+
+            $this->checks[] = $check;
+        }
     }
 
     /**
      * Get the current check result set
-     * 
+     *
      * @return array Check results
      */
     public function getChecks()
