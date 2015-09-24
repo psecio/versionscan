3b5e078027abd1a8f52ea02df151aa2df3c92c67
Chris Cornutt <enygma@phpdeveloper.org>
2015-09-24 05:30:33 -0700

    updating "missing" command and composer config for HTML parsing lib

--- src/Psecio/Versionscan/Command/MissingCommand.php
+++ src/Psecio/Versionscan/Command/MissingCommand.php
@@ -10,10 +10,15 @@ use Sunra\PhpSimple\HtmlDomParser;
 
 class MissingCommand extends Command
 {
+    private $verbose = false;
+
     protected function configure()
     {
         $this->setName('missing')
             ->setDescription('Find vulnerabilities missing from current checks')
+            ->setDefinition(array(
+                // new InputOption('php-version', 'php-version', InputOption::VALUE_OPTIONAL, 'PHP version to check'),
+            ))
             ->setHelp(
                 'Find vulnerabilities missing from current checks'
             );
@@ -27,6 +32,7 @@ class MissingCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $this->verbose = $input->getOption('verbose');
         $changelog = file_get_contents('http://php.net/ChangeLog-5.php');
 
         // Get our current checks
@@ -42,6 +48,8 @@ class MissingCommand extends Command
         preg_match_all('#<section class="version" id="([0-9\.]+)">(.+?)</section>#ms', $changelog, $matches);
 
         $cveIdList = [];
+        $fixVersions = [];
+        $results = [];
 
         // print_r($matches);
         foreach ($matches[0] as $index => $match) {
@@ -60,37 +68,72 @@ class MissingCommand extends Command
 
             // print_r($cveList);
             foreach ($cveList[0] as $cveId) {
-                if (!in_array($cveId, $cveIdList)) {
-                    $cveIdList[] = $cveId;
-                    $cveDetail = $this->getCveDetail($cveId);
-
-                    $dom = HtmlDomParser::str_get_html($cveDetail);
-
-                    $cveScore = $dom->find('div.cvssbox')[0]->plaintext;
-                    $cveSummary = explode("\n", trim($dom->find('div.cvedetailssummary')[0]->plaintext))[0];
-
-                    echo '('.$cveScore.') '.$cveSummary."\n";
+                if (in_array($cveId, $checksList) === true) {
+                    continue;
                 }
+
+                $cveIdList[] = $cveId;
+                $cveDetail = $this->getCveDetail($cveId, $output);
+                if ($cveDetail === false) {
+                    continue;
+                }
+
+                $dom = HtmlDomParser::str_get_html($cveDetail);
+
+                $cveScore = $dom->find('div.cvssbox')[0]->plaintext;
+                $cveSummary = explode("\n", trim($dom->find('div.cvedetailssummary')[0]->plaintext))[0];
+
+                $output->writeLn('('.$cveScore.') fixed in '.$versionId);
+
+                if (!isset($fixVersions[$cveId])) {
+                    $fixVersions[$cveId] = [
+                        'threat' => $cveScore,
+                        'cveid' => $cveId,
+                        'summary' => trim($cveSummary),
+                        'fixVersions' => ['base' => []]
+                    ];
+                }
+                $fixVersions[$cveId]['fixVersions']['base'][] = $versionId;
             }
         }
 
-        print_r($cveIdList);
+        if (empty($fixVersions)) {
+            $output->writeLn('No missing versions/CVEs detected');
+        } else {
+            $jsonOutput = json_encode(array_values($fixVersions), JSON_PRETTY_PRINT);
+            echo $jsonOutput."\n\n";
+        }
+
+        if ($this->verbose === true) {
+            $output->writeLn('Missing records found: '.count($fixVersions));
+        }
     }
 
-    private function getCveDetail($cveId)
+    private function getCveDetail($cveId, $output)
     {
         // save the contents locally
         $cacheFile = '/tmp/cache-'.$cveId.'.txt';
 
         if (!is_file($cacheFile)) {
             // Get the info for the CVE
-            $output->writeLn('Fetching for '.$cveId);
+            $message = 'Fetching for '.$cveId;
 
             $cveUrl = 'http://www.cvedetails.com/cve-details.php?t=1&cve_id='.$cveId;
             $cveDetail = file_get_contents($cveUrl);
             file_put_contents($cacheFile, $cveDetail);
         } else {
+            $message = 'Cache found for '.$cveId;
+
             $cveDetail = file_get_contents($cacheFile);
+        }
+
+        if (strstr($cveDetail, 'Unknown CVE ID') !== false) {
+            $message .= ' (no data)';
+            $cveDetail = false;
+        }
+
+        if ($this->verbose === true) {
+            $output->writeLn($message);
         }
 
         return $cveDetail;
